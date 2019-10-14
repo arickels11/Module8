7236ecbf4eed0d9e508766e52055d1c0b6d7c647
arickels11 <arickels93@gmail.com>
2019-10-14 19:46:00 -0300

    topic 4 test pass and docstring

--- topic_4/half_birthday.py
+++ topic_4/half_birthday.py
@@ -5,6 +5,12 @@ from _datetime import datetime, timedelta
 
 
 def half_birthday(year, month, day):
+    """
+    :param year: year that includes last birthday
+    :param month: month of birthday
+    :param day: birthday day
+    :return: half birthday for most recent birthday year
+    """
     bday = datetime(year, month, day)
     half_bday = bday + timedelta(days=184)
     return str(half_bday)
