46582b6f291ba571fc12a55bbb74f9cf9c0a7d7e
arickels11 <arickels93@gmail.com>
2019-10-14 19:44:32 -0300

    topic 4 test pass

--- topic_4/half_birthday.py
+++ topic_4/half_birthday.py
@@ -1,11 +1,14 @@
 """CIS 189
 Alex Rickels
 Mod 8 topic 4 datetime assignment"""
-import datetime
+from _datetime import datetime, timedelta
 
-def half_birthday(birthday):
-    pass
+
+def half_birthday(year, month, day):
+    bday = datetime(year, month, day)
+    half_bday = bday + timedelta(days=184)
+    return str(half_bday)
 
 
 if __name__ == '__main__':
-    print(half_birthday())
+    print(half_birthday(2019, 3, 7))
